62856a0e38f9b8a0ec890db4a588284d7fe10a53
Tejasvi <ifonlyisentthis@gmail.com>
2026-02-13 01:24:55 +0530

    fix: revert MainActivity to fix build error (splashScreen API not available)

--- android/app/src/main/kotlin/com/afterword/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/afterword/app/MainActivity.kt
@@ -1,16 +1,5 @@
 package com.afterword.app
 
-import android.os.Build
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // Immediately dismiss the Android 12+ system splash screen
-        // so only the Flutter splash screen is visible.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setKeepOnScreenCondition { false }
-        }
-    }
-}
+class MainActivity : FlutterFragmentActivity()
